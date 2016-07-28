1e29d78642224f28bbe94a18556b56f3093c3764
Norman Rzepka <code@normanrzepka.de>
2016-07-28 11:57:39 +0200

    Implemented AND queries

--- query.py
+++ query.py
@@ -1,4 +1,4 @@
-from stemming.porter2 import stem
+from preprocess import tokenize_and_stem
 import pickle
 
 with open("index.pickle", "rb") as f:
@@ -7,7 +7,13 @@ with open("doc_titles.pickle", "rb") as f:
     doc_titles = pickle.load(f)
 
 while True:
-    search_token = stem(input("Query: ").strip().lower())
-    results = [doc_titles[doc_no] for doc_no in index[search_token]]
-    print(results)
-    print(len(results))
+    search_tokens = tokenize_and_stem(input("Query: "))
+    all_results = []
+    for i, token in enumerate(search_tokens):
+        if i == 0:
+            all_results = index[token]
+        else:
+            all_results = [doc_no for doc_no in index[token] if doc_no in all_results]
+
+    print([doc_titles[doc_no] for doc_no in all_results])
+    print(len(all_results))
