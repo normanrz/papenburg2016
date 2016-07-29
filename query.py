ecbdc00b2986c1217beba2d263585c1e91fb68a0
Norman Rzepka <code@normanrzepka.de>
2016-07-29 17:50:30 +0200

    Vector space model implementation

--- query.py
+++ query.py
@@ -8,12 +8,24 @@ with open("doc_titles.pickle", "rb") as f:
 
 while True:
     search_tokens = tokenize_and_stem(input("Query: "))
-    all_results = []
-    for i, token in enumerate(search_tokens):
-        if i == 0:
-            all_results = index[token]
-        else:
-            all_results = [doc_no for doc_no in index[token] if doc_no in all_results]
 
-    print([doc_titles[doc_no] for doc_no in all_results])
-    print(len(all_results))
+    # Perform OR query
+    all_docs = []
+    for i, token in enumerate(search_tokens):
+        all_docs += index[token]
+    all_docs = set(all_docs)
+
+    # Calculate dot products
+    dot_docs = {}
+    for doc_no in all_docs:
+        # dot = sum([index[token].count(doc_no) for token in search_tokens])
+        dot = 0
+        for token in search_tokens:
+            dot += index[token].count(doc_no)
+        dot_docs[doc_no] = dot
+
+    # Sort by dot product
+    ranked_docs = sorted(dot_docs, key=dot_docs.get, reverse=True)
+
+    print([doc_titles[doc_no] for doc_no in ranked_docs])
+    print(len(ranked_docs))
